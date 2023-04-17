b79f5c1bd17296813d22461152fbbd40f38e9b5e
RBenzin <83594506+RBenzin@users.noreply.github.com>
2023-04-17 21:36:39 +0200

    use case implemented

---
+++ src/messenger.py
@@ -0,0 +1,170 @@
+import paho.mqtt.client as mqtt
+import json
+from multiprocessing import Process, Queue
+import paho.mqtt.subscribe as subscribe
+import paho.mqtt.publish as publish
+import os
+
+class Messenger():
+    def __init__(self):
+        self.connected = False
+        self.mailAddress = None
+
+        #aufbau der MQTT-Verbindung
+        self.mqttConnection = mqtt.Client()
+
+        self.mqttConnection.on_connect = self.__onConnectMQTT
+        self.mqttConnection.on_message = self.__onMessageMQTT
+
+        self.mqttConnection.message_callback_add("stt", self.__sttMQTTCallback)
+        self.mqttConnection.message_callback_add("car/connected", self.__carConnectionCallback)
+
+    def connect(self):
+        if not self.connected:
+            try:
+                docker_container = os.environ.get('DOCKER_CONTAINER', False)
+                if docker_container:
+                    mqtt_address = "broker"
+                else:
+                    mqtt_address = "localhost"
+                self.mqttConnection.connect(mqtt_address,1883,60)
+            except:
+                return False
+            self.mqttConnection.loop_start()
+        self.connected = True
+        return True
+    
+    def disconnect(self):
+        if self.connected:
+            self.connected = False
+            self.mqttConnection.loop_stop()
+            self.mqttConnection.disconnect()
+        return True
+    
+    def __onConnectMQTT(self, client, userdata, flags, rc):
+        client.subscribe([("car/connected",0), ("stt",0)])
+
+    def __sttMQTTCallback(self,client, userdata, msg):
+        # Gather stt data
+        try:
+            sttData = json.loads(str(msg.payload.decode("utf-8")))
+        except:
+            print("Can't decode message")
+            return(False)
+
+        activation_nouns = ["nachricht", "artikel"]
+        location_modifier = ["lokal"]
+        length_modifier_long = ['lang']
+
+        #check if stt  for this use case
+        if intersection(sttData["nouns"], activation_nouns):
+
+            # Make Request Response to location service if needed
+            location = {}
+            if intersection(sttData["adj"], location_modifier):
+                queue = Queue()
+                process = Process(target = mqttRequestResponseProzess, args = (queue, "req/location/current", None, "location/current"))
+                process.start()
+                process.join(timeout = 15)
+                process.terminate()
+                if process.exitcode == 0:
+                    try:
+                        location = json.loads(str(queue.get(timeout = 1).decode("utf-8")))
+                    except:
+                        print("Can't decode location answer")
+                        return(False)
+                else:
+                    print("Location service terminated unsucessfully")
+                    return(False)
+            
+            # Make Request Response to news service and activate tts
+            queue = Queue()
+            newsRequestPayload = {
+                'numArticles': 1,
+                'location': location
+            }
+            process = Process(target = mqttRequestResponseProzess, args = (queue, "req/news", newsRequestPayload, "news/article"))
+            process.start()
+            process.join(timeout = 15)
+            process.terminate()
+
+            if process.exitcode == 0:
+                try:
+                    newsData = json.loads(str(queue.get(timeout = 1).decode("utf-8")))
+                except:
+                    print("Can't decode news request answer")
+                    return(False)
+                if intersection(sttData["adj"], length_modifier_long):
+                    if('text' in newsData[0]):
+                        self.mqttConnection.publish("tts", newsData[0]['text'])
+                else:
+                    if('summary' in newsData[0]):
+                        self.mqttConnection.publish("tts", newsData[0]['summary'])
+            else:
+                print("News service terminated unsucessfully")
+                return(False)
+
+
+    #received default mqtt messages
+    def __onMessageMQTT(self, client, userdata, msg):
+        pass
+
+    def foreverLoop(self):
+        self.mqttConnection.loop_forever()
+
+    def __carConnectionCallback(self, client, userdata, msg):
+        self.mqttConnection.publish("tts", "Du bist mit deinem Auto verbunden. Hier eine lokale Nachricht")        
+        
+        # Make Request Response to location service
+        queue = Queue()
+        process = Process(target = mqttRequestResponseProzess, args = (queue, "req/location/current", None, "location/current"))
+        process.start()
+        process.join(timeout = 15)
+        process.terminate()
+        if process.exitcode == 0:
+            try:
+                location = json.loads(str(queue.get(timeout = 1).decode("utf-8")))
+            except:
+                print("Can't decode location answer")
+                return(False)
+        else:
+            print("Location service terminated unsucessfully")
+            return(False)
+
+        # Make Request Response to news service
+        queue = Queue()
+        newsRequestPayload = {
+            'numArticles': 1,
+            'location': location
+        }
+        process = Process(target = mqttRequestResponseProzess, args = (queue, "req/news", newsRequestPayload, "news/article"))
+        process.start()
+        process.join(timeout = 15)
+        process.terminate()
+
+        if process.exitcode == 0:
+            try:
+                newsData = json.loads(str(queue.get(timeout = 1).decode("utf-8")))
+            except:
+                print("Can't decode news request answer")
+                return(False)
+            if('summary' in newsData[0]):
+                self.mqttConnection.publish("tts", newsData[0]['summary'])  
+        else:
+            print("News service terminated unsucessfully")
+            return(False)
+
+def intersection(lst1, lst2):
+    lst3 = [value for value in lst1 if value in lst2]
+    return lst3
+
+def mqttRequestResponseProzess(q, requestTopic, requestData, responseTopic):
+    docker_container = os.environ.get('DOCKER_CONTAINER', False)
+    if docker_container:
+        mqtt_address = "broker"
+    else:
+        mqtt_address = "localhost"
+
+    publish.single(requestTopic,payload=requestData,hostname=mqtt_address,port=1883)
+    mqttResponse = subscribe.simple(responseTopic,hostname=mqtt_address,port=1883).payload
+    q.put(mqttResponse)
---
+++ tests/test_messenger.py
@@ -0,0 +1,39 @@
+from src import messenger
+from unittest.mock import patch, ANY, MagicMock
+from multiprocessing import Queue
+
+def test_connect():
+    obj = messenger.Messenger()
+    with patch.object(obj, 'mqttConnection') as mock_connect:
+        obj.connect()
+        mock_connect.connect.assert_called_with("localhost",1883,60)
+
+def test_disconnect():
+    obj = messenger.Messenger()
+    with patch.object(obj, 'connected', True), patch.object(obj, 'mqttConnection') as mock_connect:
+        obj.disconnect()
+        mock_connect.disconnect.assert_called()
+
+def test_foreverLoop():
+    obj = messenger.Messenger()
+    with patch.object(obj, 'mqttConnection') as mock_connect:
+        obj.foreverLoop()
+        mock_connect.loop_forever.assert_called()
+
+def test_onMQTTconnect():
+    obj = messenger.Messenger()
+    mock_client = MagicMock()
+    obj._Messenger__onConnectMQTT(mock_client,None,None,None)
+    mock_client.subscribe.assert_called_with([("car/connected",0), ("stt",0)])
+
+def test_onMQTTMessage():
+    obj = messenger.Messenger()
+    obj._Messenger__onMessageMQTT(MagicMock(),None,None)
+
+@patch("paho.mqtt.publish.single")
+@patch("paho.mqtt.subscribe.simple")
+def test_mqttRequestResponseProzess(mock_sub,mock_pub):
+    mock_sub.return_value = type('obj', (object,), {'payload' : 'asdf'})
+    messenger.mqttRequestResponseProzess(Queue(),"test/request","asdf","test/response")
+    mock_pub.assert_called_with("test/request",payload="asdf",hostname=ANY,port=ANY)
+    mock_sub.assert_called_with("test/response",hostname=ANY,port=ANY)
